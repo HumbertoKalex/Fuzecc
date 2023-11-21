e7dfd4e6dd90c4a7c4df8ffe28180b679892cc60
Humberto Oliveira <humberto.oliveira@hypergiant.com>
2023-11-21 13:35:41 -0300

    Teste

--- matches/src/main/java/com/example/teamdetail/view/TeamDetailViewModel.kt
+++ matches/src/main/java/com/example/teamdetail/view/TeamDetailViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.teamdetail.view
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -21,6 +22,7 @@ class TeamDetailViewModel(
             when (val response = safeRequest { teamDetailUseCase.fetchTeam(name) }) {
                 is SafeResponse.Success -> {
                     fetchTeam2(response.value.first(), name2)
+                    Log.d("Teste","teste")
                 }
 
                 is SafeResponse.GenericError -> TeamsAction.Error(response.errorBody?.error).run()
@@ -37,6 +39,7 @@ class TeamDetailViewModel(
                         team1.players ?: listOf(),
                         response.value.first().players ?: listOf()
                     ).run()
+                    Log.d("Teste","teste")
                 }
 
                 is SafeResponse.GenericError -> TeamsAction.Error(response.errorBody?.error).run()
